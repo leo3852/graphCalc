628ff76abacaa42f4e469f12e3469ccd81ac8e92
leonardo gurgitano <leonardogurgi@gmail.com>
2025-06-17 12:13:25 +0100

    solution - tailwind downgraded to fix postcss

---
+++ postcss.config.js
@@ -0,0 +1,16 @@
+// postcss.config.js
+// module.exports = {
+//   plugins: [
+//     require('@tailwindcss/postcss'),
+//     require('autoprefixer')
+//   ]
+// };
+// This configuration file sets up PostCSS to use Tailwind CSS and Autoprefixer.
+
+module.exports = {
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  }
+}
+//previous version
